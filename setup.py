60bee49bc7d255b8ecea59f2f52b2ed4662a7b01
Louie Slocombe <louies@hotmail.co.uk>
2024-11-04 20:45:22 -0700

    updated the project files

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='cp2k_noshell',
+    version='0.1.0',
+    author='Louie Slocombe',
+    author_email='louies@hotmail.co.uk',
+    description='Code for the ASE CP2K calculator but without using cp2k-shell',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/LouieSlocombe/ase-cp2k-noshell',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.8',
+    install_requires=[
+        'numpy',
+        'ase',
+    ],
+    extras_require={
+        'dev': [
+            'pytest',
+            'pytest-cov',
+        ],
+    },
+)
